dd33d7fc4b291d5e78750aa7050c30956350294c
doene <doene@LAPTOP-DG81KUBV>
2020-03-15 17:18:18 +0100

    removel of resource

--- frontend/controller/App.controller.js
+++ frontend/controller/App.controller.js
@@ -12,12 +12,62 @@ sap.ui.define([
 
 		onInit: function () {
 				this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				this.username = new sap.m.Input({
+					
+				})
+				this.password = new sap.m.Input({
+					
+				})
+				this.loginDialog = new sap.m.Dialog({
+					content:[this.username, this.password],
+
+					beginButton: new sap.m.Button({
+						type: sap.m.ButtonType.Emphasized,
+						text: "OK",
+						press: function () {
+							this.loginDialog.close();
+							this.login();
+						}.bind(this)
+					}),
+					endButton: new sap.m.Button({
+						text: "Close",
+						press: function () {
+							this.loginDialog.close();
+						}.bind(this)
+					})
+				});
 		},
 
 		onItemSelect: function (oEvent) {
 				var item = oEvent.getParameter('item');
 				var key = item.getKey();
 				this.oRouter.navTo(key, {});
+		},
+		handleLoginPress: function(oEvent){
+			this.loginDialog.open();
+		},
+		login: function(oEvent){
+			$.ajax({
+				type: "POST",
+				url: "/api/login",
+				contentType: "application/json; charset=utf-8",
+				data: JSON.stringify({
+					user: this.username.getValue(),
+					password: this.password.getValue()
+				}),
+				dataType: "json",
+				processData: false,
+				success: function (result) {
+					// process result
+					sap.ui.getCore().getModel().setHeaders({
+						"auth_guid" : result.auth_guid
+					});
+				},
+				error: function (e) {
+					 // log error in browser
+					console.log(e.message);
+				}
+			})
 		},
 		onSideNavButtonPress: function () {
 			var oToolPage = this.byId("toolPage");
